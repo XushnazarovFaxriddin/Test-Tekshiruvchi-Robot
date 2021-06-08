3676cdd5bd7afa0e333fce153aac7b69d1886212
XushnazarovFaxriddin <83649406+XushnazarovFaxriddin@users.noreply.github.com>
2021-06-08 10:11:35 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "goplmuqrbf_balkibumen";
+$password = "balkibumen";
+$db = "goplmuqrbf_balkibumen";
+$conn = new mysqli($servername, $username, $password, $db);
+?>
